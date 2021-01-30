c50a2b2929fc4c9bef8898f080aed69fb7dba5db
Solomon <solomonmeresa8@gmail.com>
2021-01-29 22:41:17 -0800

    adds s3-setup

--- server/db/CreateThoughtsTable.js
+++ server/db/CreateThoughtsTable.js
@@ -1,31 +1,29 @@
-const AWS = require("aws-sdk");
+const AWS =require('aws-sdk');
 
-// config points to local instance,
-// updates local environmental variables
 AWS.config.update({
-  region: "us-east-2",
-  endpoint: "http://localhost:8000"
-});
+    region: "us-east-2",
+    endpoint: "http://localhost:8000"
+  });
 
-const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+  const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-const params = {
+  const params = {
     TableName : "Thoughts",
     KeySchema: [       
-        { AttributeName: "username", KeyType: "HASH"},  //Partition key
-        { AttributeName: "createdAt", KeyType: "RANGE" }  //Sort key
+      { AttributeName: "username", KeyType: "HASH"},  // Partition key
+      { AttributeName: "createdAt", KeyType: "RANGE" }  // Sort key
     ],
     AttributeDefinitions: [       
-        { AttributeName: "username", AttributeType: "S" },
-        { AttributeName: "createdAt", AttributeType: "N" }
+      { AttributeName: "username", AttributeType: "S" },
+      { AttributeName: "createdAt", AttributeType: "N" }
     ],
     ProvisionedThroughput: {       
-        ReadCapacityUnits: 10, 
-        WriteCapacityUnits: 10
+      ReadCapacityUnits: 10, 
+      WriteCapacityUnits: 10
     }
-};
-// create table method using schema params
-dynamodb.createTable(params, (err, data) => {
+  };
+
+  dynamodb.createTable(params, (err, data) => {
     if (err) {
         console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
     } else {
